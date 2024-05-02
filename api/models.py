444bb45792c9adb1d77722fed4ab0c909e3447e3
1martin1 <cuatrocosmos@gmail.com>
2024-05-02 11:14:44 +0300

    add Create models for creating entities in DB

--- api/models.py
+++ api/models.py
@@ -1,6 +1,14 @@
 from datetime import date
+from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+
+class CreateUserRequestModel(BaseModel):
+    prompt: str = Field(default="поиграть в настольные игры")
+    group_id: Optional[int] = Field(default=45636106)
+    time_from: date = Field(default="2024-03-01")
+    time_to: date = Field(default="2024-03-31")
 
 
 class UserRequestModel(BaseModel):
@@ -8,7 +16,7 @@ class UserRequestModel(BaseModel):
     prompt: str
     type: int
     status: int
-    # group_id: int
+    group_id: Optional[int] = Field(default=None)
     time_from: date
     time_to: date
 
@@ -16,11 +24,21 @@ class UserRequestModel(BaseModel):
         orm_mode = True
 
 
+class CreateTaskModel(BaseModel):
+    prompt: str
+    UserRequestID: int
+    type: int
+    group_id: Optional[int] = Field(default=None)
+    time_from: date
+    time_to: date
+
+
 class TaskModel(BaseModel):
     ID: int
     prompt: str
+    UserRequestID: int
     type: int
-    # group_id: int
+    group_id: Optional[int] = Field(default=None)
     time_from: date
     time_to: date
 
@@ -29,10 +47,11 @@ class PostModel(BaseModel):
     ID: int
     text: str
     GroupID: int
-    vector: float
+    date: date
+    vector: list[float]
 
 
 class GroupModel(BaseModel):
     ID: int
     description: str
-    vector: float
+    vector: list[float]
