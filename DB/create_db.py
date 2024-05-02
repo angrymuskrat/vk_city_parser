c99cd712f91d5e0c423c979ffb555fa940de5fd9
1martin1 <cuatrocosmos@gmail.com>
2024-05-02 11:13:35 +0300

    Clean db before creating

--- DB/create_db.py
+++ DB/create_db.py
@@ -1,9 +1,13 @@
 from database import engine
 from models import Base
 
+
 def create_db():
-    # Создание таблиц в базе данных
+    with engine.connect() as connection:
+        connection.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
+
 
 if __name__ == "__main__":
     create_db()
